32aa22d345b05b3cf1eacac5d2a583f5bd6f0055
karthik7013 <karthiktumala143@gmail.com>
2026-06-26 03:39:20 +0000

    add @vercel/analytics and @vercel/speed-insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "@/components/providers";
 import { Toaster } from "@/components/toaster";
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({
   subsets: ["latin"],
@@ -66,6 +68,8 @@ export default function RootLayout({
           {children}
           <Toaster />
         </Providers>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
